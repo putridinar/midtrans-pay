b9ffbb2eb35dcd0c0ccdbc9f9d6f2dc743f62e1b
Putri Dinar <79742186+putridinar@users.noreply.github.com>
2025-03-10 02:53:34 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 export default {
     async fetch(request, env) {
-        if (request.method === 'POST' && request.url.includes('/function/midtrans')) {
+        if (request.method === 'POST' && request.url.includes('/midtrans-token')) {
             try {
                 const { order_id, amount, customer_details } = await request.json();
 
